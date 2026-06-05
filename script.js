3f64901a8fb0f0a0baf0cb9f870bbf2c38da616c
Joseph <corrymuth002@gmail.com>
2026-06-05 10:00:47 -0500

    modified image tag

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const formTag = document.getElementById("front-form");
+const formTag = document.getElementById("image-form");
 formTag.onsubmit = handleSubmit;
-const suc = document.getElementById("change");
+const suc = document.getElementById("image");
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  const values = {
-    username: form.elements.username.value,
-    password: form.elements.password.value,
-    happy: form.elements.planAdvanced1.checked,
-    sad: form.elements.planAdvanced2.checked,
+  const data = {
+    "x-api-key": form.elements.key.value,
+    limit: form.elements.amount.value,
   };
-  const isChecked2 = values.sad;
-  console.log(values.sad);
-  values.username.value = "Hello";
-  console.log(values.username.value);
-  if (isChecked2) {
-    update.innerHTML = "<h1>You can do it!</h1>";
-  } else {
-    suc.innerText = "";
+  const dataString = new URLSearchParams(data);
+  dataString.toString(data);
+  const response = await fetch(
+    "https://api.thecatapi.com/v1/images/search" + "?" + dataString,
+  );
+  console.log("https://api.thecatapi.com/v1/images/search" + "?" + dataString);
+  const receive = await response.json();
+  const funFact = receive;
+  console.log(data.limit);
+  for (var i = 0; i < data.limit; i++) {
+    const imgUrl = funFact[i].url;
+    const img = document.createElement("img");
+    img.src = imgUrl;
+    img.alt = "Cat";
+    img.width = 200;
+    img.length = 200;
+    document.body.appendChild(img);
+  }
+}
+
+function addImage() {
+  for (var i = 0; i < data.limit; i++) {
+    const imgUrl = funFact[i].url;
+    const img = document.createElement("img");
+    img.src = imgUrl;
+    img.alt = "Cat";
+    document.body.appendChild(img);
   }
 }
